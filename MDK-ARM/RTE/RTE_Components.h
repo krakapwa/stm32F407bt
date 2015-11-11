032698646201cb4578ff2b0817e94362c0f15f4c
unknown <olol85@gmail.com>
2015-11-11 12:53:48 +0100

    Project for STM32F407 discovery board and RN-42 bluetooth module.

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f407bt' 
+ * Target:  'stm32f407bt Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
